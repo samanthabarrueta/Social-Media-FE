d2f4aaf91c223e1a66b10a80f56539301a1a0b2b
Gentry Hunt <ghunt@csu.edu>
2020-09-10 17:10:45 -0500

    updated some posts on the homepage

--- react-front/src/feed/HomePosts.js
+++ react-front/src/feed/HomePosts.js
@@ -2,51 +2,92 @@ import React, { Component } from 'react';
 import '../index.css'
 
 export class HomePost extends Component {
-    // constructor() {
-    //     super()
-    //     this.state = {
-    //         name: "Samantha Barrueta",
-    //         timeStamp: "Posted at 9:00 PM",
-    //         body: "Today was awesome!"
-    //     }
-    // }
+        // constructor() {
+        //     super()
+        //     this.state = {
+        //         name: "Samantha Barrueta",
+        //         timeStamp: "Posted at 9:00 PM",
+        //         body: "Today was awesome!"
+        //     }
+        // }
 
-    render() {
-        return (
-            <div>
-                <div className= "mt-5 mb-5 card">
-                    <h5>{"Samantha Barrueta"}</h5>
-                    <h6>{"Today at 10:00 AM"}</h6>
-                    <p>{"Just landed a new job!"}</p>
-            </div>
-            <div className= "mt-5 mb-5 card">
-                    <h5>{"Mohammad Jihad Uddin"}</h5>
-                    <h6>{"Today at 12:05 PM"}</h6>
-                    <p>{"Got my first interview!"}</p>
-            </div>
-            <div className= "mt-5 mb-5 card">
-                    <h5>{"Shelsea Yilmaz"}</h5>
-                    <h6>{"Yesterday at 9:35 PM"}</h6>
-                    <p>{"I love the coding class I'm in!"}</p>
-            </div>
-            <div className= "mt-5 mb-5 card">
-                    <h5>{"Gentry Hunt"}</h5>
-                    <h6>{" Yesterday at 4:05 PM"}</h6>
-                    <p>{"PRGSSN. Check out the new merch."}</p>
-            </div>
-            <div className= "mt-5 mb-5 card">
-                    <h5>{"Mariya Kirichenko"}</h5>
-                    <h6>{"Yesterday at 4:00 PM"}</h6>
-                    <p>{"Just moved into my apartment. Any good furniture stores in the Edgewater area?"}</p>
-            </div>
-            <div className= "mt-5 mb-5 card">
-                    <h5>{"Paul Cruse"}</h5>
-                    <h6>{"Yesterday at 2:17 PM"}</h6>
-                    <p>{"Another recruiter just slid in my LinkedIn DM's "}</p>
-            </div>
-            </div>
-        )
-    }
+        render() {
+                return (
+                        <div>
+                                <div className="mt-5 mb-5 card">
+                                        <h5>{"Paul Cruse"}</h5>
+                                        <h6>{"Today at 12:11 PM"}</h6>
+                                        <p>{"Some women want designer 👠 & 👜, my girl wanted a 🏠 (and designer 👠 & 👜 lol)"}</p>
+                                </div>
+                                <div className="mt-5 mb-5 card">
+                                        <h5>{"Samantha Barrueta"}</h5>
+                                        <h6>{"Today at 10:00 AM"}</h6>
+                                        <p>{"Just landed a new job!"}</p>
+                                </div>
+                                <div className="mt-5 mb-5 card">
+                                        <h5>{"Mohammad Jihad Uddin"}</h5>
+                                        <h6>{"Today at 12:05 PM"}</h6>
+                                        <p>{"Got my first interview!"}</p>
+                                </div>
+                                <div className="mt-5 mb-5 card">
+                                        <h5>{"Shelsea Yilmaz"}</h5>
+                                        <h6>{"Yesterday at 9:35 PM"}</h6>
+                                        <p>{"I love the coding class I'm in!"}</p>
+                                </div>
+                                <div className="mt-5 mb-5 card">
+                                        <h5>{"Gentry Hunt"}</h5>
+                                        <h6>{" Yesterday at 4:05 PM"}</h6>
+                                        <p>{"PRGSSN. Check out the new merch."}</p>
+                                </div>
+                                <div className="mt-5 mb-5 card">
+                                        <h5>{"Mariya Kirichenko"}</h5>
+                                        <h6>{"Yesterday at 4:00 PM"}</h6>
+                                        <p>{"Just moved into my apartment. Any good furniture stores in the Edgewater area?"}</p>
+                                </div>
+                                <div className="mt-5 mb-5 card">
+                                        <h5>{"Paul Cruse"}</h5>
+                                        <h6>{"Yesterday at 2:17 PM"}</h6>
+                                        <p>{"Another recruiter just slid in my LinkedIn DM's "}</p>
+                                </div>
+                                <div className="mt-5 mb-5 card">
+                                        <h5>{"Paul Cruse"}</h5>
+                                        <h6>{"Yesterday at 2:17 PM"}</h6>
+                                        <p>{"You never ever ever want to slide in a FAANG's DMs like yooooo I just got out of a bootcamp and I'm ready because I can guarantee you're not"}</p>
+                                </div>
+                                <div className="mt-5 mb-5 card">
+                                        <h5>{"Paul Cruse"}</h5>
+                                        <h6>{"Yesterday at 2:17 PM"}</h6>
+                                        <p>{"Study! Netflix is going to be around in 6 months from now."}</p>
+                                </div>
+                                <div className="mt-5 mb-5 card">
+                                        <h5>{"Paul Cruse"}</h5>
+                                        <h6>{"Yesterday at 2:17 PM"}</h6>
+                                        <p>{"Fruit snacks be life"}</p>
+                                </div>
+                                <div className="mt-5 mb-5 card">
+                                        <h5>{"Julian Craigwell-Graham"}</h5>
+                                        <h6>{"Yesterday at 2:17 PM"}</h6>
+                                        <p>{"Let's wait for Paul to join in."}</p>
+                                </div>
+                                <div className="mt-5 mb-5 card">
+                                        <h5>{"Jaime Araujo"}</h5>
+                                        <h6>{"Yesterday at 2:17 PM"}</h6>
+                                        <p>{"Don't forget to check into BCS!"}</p>
+                                </div>
+                                <div className="mt-5 mb-5 card">
+                                        <h5>{"Jaime Araujo"}</h5>
+                                        <h6>{"Yesterday at 2:17 PM"}</h6>
+                                        <p>{"Check Gitlab for your homework"}</p>
+                                </div>
+                                <div className="mt-5 mb-5 card">
+                                        <h5>{"Paul Cruse"}</h5>
+                                        <h6>{"Yesterday at 2:17 PM"}</h6>
+                                        <p>{"Sometimes, you got to get lost in the sauce."}</p>
+                                </div>
+                                
+                        </div>
+                )
+        }
 }
 
 export default HomePost;
